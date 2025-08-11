bda3a90bee121f11ebd600705e8c45b22af4c427
Q Sivakorn Tuangwilai <sivakorn.tuangwilai@mds.ac.nz>
2025-08-12 03:17:12 +1200

    Texture1

--- OpenGL_Project/OpenGL_Project/main.cpp
+++ OpenGL_Project/OpenGL_Project/main.cpp
@@ -3,6 +3,8 @@
 #include <glm.hpp>
 #include <gtc/matrix_transform.hpp>
 #include <gtc/type_ptr.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
 GLFWwindow* Window = nullptr;
 
@@ -18,11 +20,11 @@ GLfloat Vertices_Tri[] = {
 	0.5f, 0.8f, 0.0f,	0.0f, 0.0f, 1.0f,	// Bottom Center
 };
 GLfloat Vertices_Quad[] = {
-	// Index	// Position			// Color
-	/*0*/		-0.5f, 0.5f, 0.0f,	1.0f, 0.0f, 0.0f,	// Top Left
-	/*1*/		-0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f,	// Btm Left
-	/*2*/		0.5f, -0.5f, 0.0f,	0.0f, 0.0f, 1.0f,	// Btm Right
-	/*3*/		0.5f, 0.5f, 0.0f,	0.0f, 1.0f, 1.0f,	// Top Right
+	// Index	// Position			// Color			// Texture Coords
+	/*0*/		-0.5f, 0.5f, 0.0f,	1.0f, 0.0f, 0.0f,	-2.0f, 2.0f,		// Top Left
+	/*1*/		-0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f,	-2.0f, -2.0f,		// Btm Left
+	/*2*/		0.5f, -0.5f, 0.0f,	0.0f, 0.0f, 1.0f,	2.0f, -2.0f,		// Btm Right
+	/*3*/		0.5f, 0.5f, 0.0f,	0.0f, 1.0f, 1.0f,	2.0f, 2.0f,		// Top Right
 };
 GLuint Indices_Quad[] = {
 	0, 1, 2, // First Triangle (TL > BL > BR)
@@ -48,18 +50,18 @@ GLuint Indices_Hex[] = {
 	6, 0, 2, // Sixth Triangle (TR > TL > C)
 };
 // Object Matrices and Components -------------------
-glm::vec3 Position = glm::vec3(-0.5f, -0.5f, 0.0f);
+glm::vec3 Position = glm::vec3(0.0f, 0.0f, 0.0f);
 glm::mat4 TranslationMat;
 glm::vec3 PositionSecondHex = glm::vec3(0.5f, 0.5f, 0.0f);
 glm::mat4 TranslationMatSecondHex;
 float RotationAngle = 0.0f; // Degrees
 glm::mat4 RotationMat;
-glm::vec3 Scale = glm::vec3(0.5f, 0.5f, 1.0f);
+glm::vec3 Scale = glm::vec3(1.5f, 1.5f, 1.0f);
 glm::mat4 ScaleMat;
 glm::vec3 SolidColorRed = glm::vec3(1.0f, 0.0f, 0.0f); // Red
 glm::vec3 SolidColorGreen = glm::vec3(0.0f, 1.0f, 0.0f); // Green
 
-
+GLuint texturePotion;
 //--------------------------------------------------
 
 int main()
@@ -122,7 +124,7 @@ void Render(Program* _program, float* _currentTime)
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	// Program to use
-	GLuint ProgramToUse = _program->Program_Assignment1;
+	GLuint ProgramToUse = _program->Program_Texture;
 
 	// Bind the Program
 	glUseProgram(ProgramToUse);
@@ -141,15 +143,21 @@ void Render(Program* _program, float* _currentTime)
 	glUniformMatrix4fv(ScaleMatLoc, 1, GL_FALSE, glm::value_ptr(ScaleMat));
 	GLint SolidColorLoc = glGetUniformLocation(ProgramToUse, "SolidColor");
 	glUniform3fv(SolidColorLoc, 1, glm::value_ptr(SolidColorRed));
+
+	// Activate and bind the texture
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texturePotion);
+	glUniform1i(glGetUniformLocation(_program->Program_Texture, "Texture0"), 0);
 	
 	// Render
-	glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0); //Hexagon using EBO
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); //Quad using EBO
+	//glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0); //Hexagon using EBO
 
-	// Second Hexagon -----------------------------
-	glUniformMatrix4fv(TranslationMatLoc, 1, GL_FALSE, glm::value_ptr(TranslationMatSecondHex));
-	glUniform3fv(SolidColorLoc, 1, glm::value_ptr(SolidColorGreen));
-	// Render the second hexagon
-	glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0); //Hexagon using EBO
+	//// Second Hexagon -----------------------------
+	//glUniformMatrix4fv(TranslationMatLoc, 1, GL_FALSE, glm::value_ptr(TranslationMatSecondHex));
+	//glUniform3fv(SolidColorLoc, 1, glm::value_ptr(SolidColorGreen));
+	//// Render the second hexagon
+	//glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0); //Hexagon using EBO
 	
 	//Unbind the VAO and program to prevent accidental modifications
 	glBindVertexArray(0);
@@ -177,33 +185,64 @@ void InitialSetup(Program* _program)
 																"Resources/Shaders/VertexColorFade.frag");
 	_program->Program_Assignment1 = ShaderLoader::CreateProgram("Resources/Shaders/Assignment1_WorldSpace.vert",
 																"Resources/Shaders/Assignment1_Color.frag");
+	_program->Program_Texture = ShaderLoader::CreateProgram("Resources/Shaders/Texture.vert",
+															"Resources/Shaders/Texture.frag");
     
 	// Generate VAO
 	glGenVertexArrays(1, &_program->VAO_Tri);
 	glBindVertexArray(_program->VAO_Tri);
 	
 	//Generate EBO for the quad
-	/*glGenBuffers(1, &_program->EBO_Quad);
+	glGenBuffers(1, &_program->EBO_Quad);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _program->EBO_Quad);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices_Quad), Indices_Quad, GL_STATIC_DRAW);*/
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices_Quad), Indices_Quad, GL_STATIC_DRAW);
 	//Generate EBO for the hexagon
-	glGenBuffers(1, &_program->EBO_Hex);
+	/*glGenBuffers(1, &_program->EBO_Hex);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _program->EBO_Hex);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices_Hex), Indices_Hex, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices_Hex), Indices_Hex, GL_STATIC_DRAW);*/
 
 	//Generate VBO
 	glGenBuffers(1, &_program->VBO_Tri);
 	glBindBuffer(GL_ARRAY_BUFFER, _program->VBO_Tri);
 	//glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices_Tri), Vertices_Tri, GL_STATIC_DRAW); //Triangle
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices_Quad), Vertices_Quad, GL_STATIC_DRAW); //Quad
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices_Hex), Vertices_Hex, GL_STATIC_DRAW); //Hexagon
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices_Quad), Vertices_Quad, GL_STATIC_DRAW); //Quad
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices_Hex), Vertices_Hex, GL_STATIC_DRAW); //Hexagon
 
 	// Set the vertex attribute pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(2);
 	glBindVertexArray(0);
+
+	// TEXTURE SETUP ---------------------------------
+	// Load the image data
+	int imageWidth, imageHeight, imageComponents;
+	stbi_set_flip_vertically_on_load(true); // Flip the image vertically if needed
+	unsigned char* imageData = stbi_load(	"Resources/Textures/Jump_Attack__000.png",
+											&imageWidth, &imageHeight, &imageComponents, 0);
+	if (!imageData) {
+		std::cerr << "Failed to load texture image!" << std::endl;
+	}
+	// Create and bind a texture
+	glGenTextures(1, &texturePotion);
+	glBindTexture(GL_TEXTURE_2D, texturePotion);
+	// Check how many components the image has (RGBA or RGB)
+	GLint loadedComponents = (imageComponents == 4) ? GL_RGBA : GL_RGB;
+	// Populate the texture with the image data
+	glTexImage2D(	GL_TEXTURE_2D, 0, loadedComponents, imageWidth, imageHeight, 0,
+					loadedComponents, GL_UNSIGNED_BYTE, imageData);
+	// Setting the filtering
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	// Generate the mipmaps, free the memory, and unbind the texture
+	glGenerateMipmap(GL_TEXTURE_2D);
+	stbi_image_free(imageData);
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 void Update(float* _currentTime)
